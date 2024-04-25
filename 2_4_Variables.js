072f0022af3430ec20b39f6778ef5fce64d258d2
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-04-25 03:09:56 -0700

    2_4_Variables.js

--- 2_4_Variables.js
+++ 2_4_Variables.js
@@ -2,3 +2,31 @@
 
 let msg = "This is a test message";
 console.log("The test message output: " + msg);
+
+//we can use different name of variable in multiple line:
+let userName = "Ahmed";
+let age = 5;
+let message = 'Ahmed is five years old, He read in class Prep-1';
+console.log(userName, age, message);
+
+//we can use multiple variables in one line :
+let name = "Rahim" , ageNum = 3 , msge = "He is not going school yet";
+console.log(name, ageNum, msge);
+
+//we can change the value or variable put in another 
+let value;
+value = 4;
+value = 2;
+console.log(value);
+
+//we can declared two variables and copy data from one into the other 
+//  let firstName = "Ahmed";
+//  lastName = "Hassan";
+//  console.log(firstName);
+//  console.log(lastName);
+ 
+
+//To declare a constant (unchanging) variable, use const instead of let:
+const dateOfBirth = '08.05.1985';
+console.log(dateOfBirth);
+
