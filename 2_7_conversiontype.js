8a648345281a529f83cbfb13f8eeb9b05c907c9d
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-04-26 05:19:04 -0700

    init commit
    update and commit changes

---
+++ 2_7_conversiontype.js
@@ -0,0 +1,40 @@
+"use strict"
+//string conversion
+let name = "abc";
+console.log(typeof name);
+
+//numeric conversion 
+let str = "123";
+console.log(typeof str);
+
+let num = 123;
+console.log(typeof num);
+
+
+
+
+
+
+//boolean conversion
+let value = 0;
+console.log(Boolean(value));
+
+let val = 1;
+console.log(Boolean(val));
+
+let msg = "Hello";
+console.log(Boolean(msg));
+
+let no = ("");
+console.log(Boolean(no));
+
+
+
+
+
+
+
+
+
+
+
--- 2_8_BasicOperators.js
+++ 2_8_BasicOperators.js
@@ -8,4 +8,24 @@ console.log(c);
 let d = b++; 
 console.log(d);
 
+//Basic Operator Math
+//Unary operands if its single
+let r = 1;
+r = -r;
+console.log(r);
+
+//The binary operands has two operands
+
+let x = 2 , y = 4;
+console.log(y - x);
+
+let e = 2 , f = 4;
+console.log(e + f);
+
+//Remainder
+let g = 5, h = 6;
+console.log(h % g);
+
+
+
 
