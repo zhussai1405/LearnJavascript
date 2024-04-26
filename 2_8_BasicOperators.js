3d613d480827e9990a158b5398b55ba73a29b55c
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-04-26 09:28:17 -0700

    2_8_BasicOperators.js

--- 2_8_BasicOperators.js
+++ 2_8_BasicOperators.js
@@ -26,6 +26,59 @@ console.log(e + f);
 let g = 5, h = 6;
 console.log(h % g);
 
+//Exponentation 
+console.log(2**2);
+console.log(2**3);
+//squareroot or cubeic root
+console.log(2**(1/2));
+console.log(2**(1/3));
+
+//string concatenation with binary +
+let name = "Faraz"+"Hassan";
+console.log(name);
+
+let str = "1";
+let num = 2;
+console.log("1" + 2);
+
+let s = 1;
+let n = "2";
+console.log(2 + "1");
+
+//Unary conversion unary +
+let i = 1;
+console.log(+i);
+
+let j = -2;
+console.log(+j);
+
+//Example 2
+
+let apple = "2";
+let oranges = "3";
+console.log(+apple+  +oranges);
+
+//Assignment return a value 
+let k =1;
+let l = 2;
+let m = 3 -(k = l + 1);
+console.log(k);
+console.log(l);
+
+// Modify in Place
+let numb = 2;
+numb +=5;
+numb *=4;
+console.log(numb);
+
+//Increment / Decrement 
+let counter = 2;
+counter++;
+console.log(counter);
+
+let count = 2;
+count--;
+console.log(count);
 
 
 
