6e950d84baec2c315c7c482d6c2a62946eb200ad
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-04-27 03:33:50 -0700

    2_10_ifelse.js

---
+++ 2_10_ifelse.js
@@ -0,0 +1,43 @@
+"use strict"
+// let isGreater = (5 > 4);
+// if(5 > 4){
+// console.log(isGreater);
+// }else{
+// console.log(isGreater);
+// }
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+let year = 2015;
+
+if (year >= 2015) {
+  console.log("You are right");
+} else {
+  console.log("You are wrong.");
+}
+  
+let num = (5 , 4);
+
+if (5 < 4) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+let cond = (0 , 1);
+
+if (0 == 1) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+
+
+
+    
---
+++ 2_9_Comparison.js
@@ -0,0 +1,22 @@
+"use strict"
+
+//Boolean comparison 
+let result = (5 > 4);
+console.log("check greater num and result in boolean:" + result);
+
+let answer = (4 < 5);
+console.log("check greater num and result in boolean:" + result);
+
+
+let fruit = ("apple" > "pineapple");
+console.log("According to the alphabeticaly pineapple is > apple: " + fruit);
+
+let strGreater = ("2" > "12");
+console.log("Strings number comparison 2 > first char of compare num 1:" + strGreater);
+
+//Equality comparison to check 
+console.log(undefined == null);
+
+//Strict equality comparison to check 
+console.log(undefined === null);
+
