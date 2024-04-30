dbc35ddd2d41916fef973a4b0ea8e84450497c1b
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-04-30 05:09:30 -0700

    init commit
    commit changes loops

--- 2_11_LogicalOperators.js
+++ 2_11_LogicalOperators.js
@@ -1,4 +1,5 @@
 "use strict"
+//Logical Operator with OR ||.
 let hour= 9;
 if(hour < 8 || hour > 18){
 console.log('Office is Closed');
@@ -8,4 +9,11 @@ console.log('Office is Closed');
 let firstName = "";
 let lastName = "";
 let nickName = "SuperCoder";
-    console.log(firstName || lastName || nickName || "Anonymous");
+console.log(firstName || lastName || nickName || "Anonymous");
+
+//Exapmple with AND &&.
+let hours = 12;
+let minutes = 30;
+if(hours == 12 && minutes == 30){
+    console.log('Time is 12:30');
+}
---
+++ 2_13_Loops.js
@@ -0,0 +1,30 @@
+"use strict"
+//while loop
+let i = 0;
+while (i < 3) { 
+  console.log( i );
+  i++;
+}
+
+// when j become 0 condition become falsy
+let j = 3;
+while (j) { 
+  console.log( j );
+  j--;
+}
+
+// Do while loop 
+let k = 0;
+do {
+  console.log( k );
+  k++;
+} while (k < 3);
+
+//for loop
+let l = 0;
+
+for (l = 0; l < 3; l++) { 
+  console.log(l); 
+}
+
+console.log(l); 
