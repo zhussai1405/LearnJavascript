4ed1821fd544c886233611191573c94b35feec7d
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-05-04 02:35:24 -0700

    2_15_Function.js

--- 2_15_Function.js
+++ 2_15_Function.js
@@ -9,6 +9,39 @@ function fruit(){
 
 fruit();
 
+//Function declaration 
+let firstName = "Ahmed";
+let lastName = "Hassan";
+
+function greet(){
+   console.log("Hello" + firstName," " + lastName);
+}
+greet();
+
+
+function age(){
+   let age1 = 36;
+   let age2 = 38;
+   console.log("age after two years, " + age1," "+age2);
+}
+age();
+
+//Function Declaration
+function greet(name) {
+   return(`Hello, ${name}!`);
+ }
+ //Calling the function
+ console.log(greet("Rahim Hassan"));
+
+ //Function Expression:
+ const name = function(name) {
+   return `Hello, ${name}!`;
+ };
+ 
+ // Calling the function
+ console.log(name("Ahmed Hassan")); 
+ 
+
 //Local variable
 function fruit(){
     let b = "banana";
