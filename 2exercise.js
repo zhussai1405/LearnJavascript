6d25c2ed1b7ab6df46318e0c81a3414512b78eb2
zhussai1 <zhussai1@gmail.com>
2024-05-20 13:00:00 -0400

    Exercises
    New Exercises

---
+++ 2exercise.js
@@ -0,0 +1,12 @@
+//Exercise 2 - Requirements
+//
+//All code should have appropriate logging
+//
+//Write a function that reverses a word
+//the function takes a single parameter called word
+//the function returns the reversed word 
+//
+//You must convert the string to an array and cannot use any functions do that
+//use a for loop to reverse the string
+//Take the elements of the array and reverse them and store them in a variable
+//the function needs to return the variable.
---
+++ 4exercise.js
@@ -0,0 +1,9 @@
+//Exercise 4 - Requirements
+//
+//All code should have appropriate logging
+//
+//Write a function that converts the first letter of a word to uppercase and return the word
+//Example word: lrgstNbr(3,6,9) -> 9 or (4,10,6) -> 10
+//Name the function appropriately
+//The function will have 3 parameters
+//
