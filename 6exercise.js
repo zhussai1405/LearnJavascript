b739493ca66bce248d5a9b138f1e12a51fffe5c9
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-06-10 10:48:59 -0700

    init commit
    commit changes

--- 6exercise.js
+++ 6exercise.js
@@ -5,9 +5,19 @@
 // Assign your name (string), age (number), and student status (boolean)
 // Log all three variables to the console
 
+// Reference Q in line 4.
+let person = 'John';
+let age = 30;
+let isStudent = true;   //boolean yes or no
+console.log(person,age,isStudent);
+
 //Question 2
 // Declare a constant variable PI and assign it 3.14159
 // Try to reassign a new value to PI and observe the error
+const PI = 3.14159;
+//PI = 2.14;
+console.log(PI);
+//It going to be error, because in const variable we cannot reassign a new value.
 
 
 //Question 3
@@ -16,11 +26,35 @@
 // Call the function and log the new globalVar value
 // Declare a local variable localVar inside the function with value 20
 // Try to log localVar outside the function - explain what will happen
+let globalVar = 10;
+function updateGlobalVar(){
+    globalVar += 5;
+    
+
+}
+updateGlobalVar();
+console.log(globalVar);
+
+//Now declare a local variable
+function updateLocalVar(){
+    let localVar = 20;
+    console.log(localVar);
+    
+}
+updateLocalVar();
 
 //Question 4
 // Log the value of x (before declaring it)
 // Declare a variable x and assign it 5
 // Explain the output
+
+
+//before declaring log given undefined value is s
+let x;
+x = 5;
+console.log(x);
+
+
 
 //Question 5
 // Declare a variable x with value 10 outside a function
@@ -29,7 +63,22 @@
 // Call the function and observe the output
 // Explain what shadowing or masking of variables is
 
+let x_ = 10;
+function shadowing(){
+    let x_ = 20;
+    console.log(x_);
+}
+shadowing();
+
+
+
 //Question 6
 // Declare variables a and b with values 5 and 10
 // Swap the values of a and b using a temporary variable
 // Log the values of a and b before and after swapping
+
+let a = 5;
+let b = 10;
+console.log(`a=${a}, b = ${b}`);
+
+
--- 7exercise.js
+++ 7exercise.js
@@ -6,7 +6,16 @@
 // Declare a variable with a very large integer value (e.g., 9007199254740991)
 // Check its type and explain the output
 
+let x = 5;
+console.log(typeof x);      //number
 
+
+let y = Infinity;
+console.log(typeof y);      //number
+
+
+const z = 9007199254740991;
+console.log(typeof z);          //number
 
 
 
@@ -19,7 +28,14 @@
 // Try assigning a regular number and see what happens
 // Perform arithmetic operations (addition, subtraction, multiplication) with BigInt values
 
-
+let bigint = 1234567890;
+let reguNum = 50;
+let sum = bigint + reguNum;
+console.log(sum);
+let sub = bigint - reguNum;
+console.log(sub);
+let multip = bigint * reguNum;
+console.log(multip);
 
 
 
@@ -31,6 +47,15 @@
 // Try concatenating strings using + operator
 // Check the type of a string variable
 
+let singleQuotes = 'Single quots';
+let doubleQuotes = "Double Quotes";
+let backTicks = `Backticks`
+
+let singleQuoStr = 'Faraz';
+let doubleQuoStr = "Hassan";
+console.log(`embed the Concate single quote and double quote : " ${singleQuoStr} , ${doubleQuoStr}`);
+console.log("concate the : " , singleQuoStr + doubleQuoStr);
+        
 
 
 
@@ -38,6 +63,46 @@
 // Declare variables with boolean values (true and false)
 // Use comparison operators (>, <, ===, etc.) to get boolean results
 // Combine boolean values using logical operators (&&, ||, !)
+
+let num = (5 , 4);
+
+//comparison operator to get boolean result
+//comaprison with greater sign
+if (5 < 4) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+//comparison with less than sign 
+if (4 > 5){
+    console.log("true");
+}else{
+    console.log('false')
+}
+//comparison with strict equality check
+if (5 === 4){
+    console.log("true")
+}else{
+    console.log("false")
+}
+// comaprison with and && operator sign
+if (5 == 5 && 3 == 3){
+    console.log("true")
+}else{
+    console.log("false")
+}
+//comparison with OR || Operator sign 
+if (5 == 4 || 2 == 3){
+    console.log("true")
+}else{
+    console.log("false")
+}
+//comparison with Not! operator sign 
+if (5 != 4){
+    console.log("true");
+}else{
+    console.log("false")
+}
 
 
 
@@ -50,6 +115,23 @@
 // Assign null to a variable and check its type
 // Try arithmetic operations with null and undefined values
 
+let a;
+console.log(typeof a);
+
+let b = null;
+console.log(b);
+
+//arithmetic operator with null
+console.log(null + 1);
+console.log(null - 1);
+console.log(null * 1);
+console.log(null / 1);
+
+//arithmetic operator with undefined 
+console.log(undefined + 1);
+console.log(undefined - 1);
+console.log(undefined * 1);
+console.log(undefined / 1);
 
 
 
@@ -64,16 +146,19 @@
 // Convert a non-boolean value to a boolean using !!
 // Explore other type conversion methods (e.g., Boolean(), toString())
 
+let str = "123";
+let numb = Number(str);
+console.log("string convert into a number : " + numb);
 
 
+let number = 123;
+let string = String(number); 
+console.log("number convert into a string : " + string);
 
 
-
-
-
-
-
-
+let nonBooleanValue = "Hello";
+let booleanValue = !!nonBooleanValue;
+console.log(booleanValue);
 
 
 
@@ -83,20 +168,74 @@
 // Try typeof on variables, literals, expressions, and functions
 // Explain the output when using typeof on null
 
+let value = 2;
+console.log(typeof value);
+
+
+let val = "Hello to EveryOne";
+console.log(typeof val);
+
+
+let value_ = true;
+console.log(typeof value_);
 
 
 
 
 
 
+let valu = null;
+console.log(typeof valu);
+
+// types of Variable 
+let c = 5;
+var d = 10; 
+const e = 15;
+
+
+//INteger literals 
+let age = 39;
+
+// Floating points
+let prince = 2.15;
+
+//string literals
+let greet = "Hello";
+
+
+//Boolean literals 
+let isUserLogin = true;
+
+
+//Expression arithmetic 
+let sumOf = 3+4;
+
+//Logical expression 
+let sumOfTwoNum = 3 + 4;
+let maximum =  Math.max(a + b)
+console.log(sumOfTwoNum);
 
 
 
+// function define 
+function add(a,b){
+    return a + b;
+}
+console.log(add(2 , 2));
+
+//function expression 
+const subtract = function(a, b){
+    return a - b;
+
+}
+console.log(subtract(2 , 3));
 
 
-
-
-
+//OUtput using type of null
+let value1= null;
+if (value1 === null){
+    console.log(value1);
+}
 
 
 
@@ -105,3 +244,28 @@
 // Declare variables of different types (string, number, boolean)
 // Perform operations involving mixed types (e.g., adding a number and string)
 // Observe and explain the results
+
+
+
+//string variable 
+let name = "faraz";
+let greetings  = "Hello nice to meet you";
+
+
+
+//number variables
+let agee = 6;
+let PI = 3.14;
+
+
+//Boolean variable 
+let isUserDefined = true;
+let isLoggedIn = false;
+
+
+//Mixed type conversion 
+let nbr = 10;
+let strng = "10";
+let concat = nbr + strng;
+console.log(concat);
+
